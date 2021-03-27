205fcff86e4fbbf96eb8a95c28242762b32aab21
emilykmcullen <emilycullen135@gmail.com>
2021-03-27 11:01:10 +0000

    sprites added

--- TargetGame/Game1.cs
+++ TargetGame/Game1.cs
@@ -9,6 +9,13 @@ namespace TargetGame
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Texture2D targetSprite;
+        Texture2D crosshairsSprite;
+        Texture2D backgroundSprite;
+        SpriteFont gameFont;
+
+        Vector2 targetPosition = new Vector2(300, 300);
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -27,7 +34,11 @@ namespace TargetGame
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            targetSprite = Content.Load<Texture2D>("target");
+            crosshairsSprite = Content.Load<Texture2D>("crosshairs");
+            backgroundSprite = Content.Load<Texture2D>("sky");
+            gameFont = Content.Load<SpriteFont>("galleryFont");
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -44,7 +55,11 @@ namespace TargetGame
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
+            _spriteBatch.DrawString(gameFont, "Test message", new Vector2(100,100), Color.White);
+            _spriteBatch.Draw(targetSprite, targetPosition, Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
