b3a4587c27c4600962f37ebe6b39e162f641fc81
emilykmcullen <emilycullen135@gmail.com>
2021-03-27 12:30:02 +0000

    game working

--- TargetGame/Game1.cs
+++ TargetGame/Game1.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+
 
 namespace TargetGame
 {
@@ -15,18 +17,23 @@ namespace TargetGame
         SpriteFont gameFont;
 
         Vector2 targetPosition = new Vector2(300, 300);
+        const int targetRadius = 45;
+
+        MouseState mState;
+        bool mReleased = true;
+        int score = 0;
+
+        double timer = 10;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
@@ -46,7 +53,37 @@ namespace TargetGame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if (timer > 0)
+            {
+                timer -= gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (timer < 0)
+            {
+                timer = 0;
+            }
+
+            mState = Mouse.GetState();
+
+            if (mState.LeftButton == ButtonState.Pressed && mReleased == true)
+            {
+                float mouseTargetDist = Vector2.Distance(targetPosition, mState.Position.ToVector2());
+                if (mouseTargetDist < targetRadius && timer > 0)
+                {
+                    score++;
+
+                    Random rand = new Random();
+
+                    targetPosition.X = rand.Next(0, _graphics.PreferredBackBufferWidth);
+                    targetPosition.Y = rand.Next(0, _graphics.PreferredBackBufferHeight);
+                }
+                mReleased = false;
+            }
+
+            if (mState.LeftButton == ButtonState.Released)
+            {
+                mReleased = true;
+            }
 
             base.Update(gameTime);
         }
@@ -57,8 +94,16 @@ namespace TargetGame
 
             _spriteBatch.Begin();
             _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
-            _spriteBatch.DrawString(gameFont, "Test message", new Vector2(100,100), Color.White);
-            _spriteBatch.Draw(targetSprite, targetPosition, Color.White);
+            _spriteBatch.DrawString(gameFont, "Score: " + score.ToString(), new Vector2(3,3), Color.White);
+            _spriteBatch.DrawString(gameFont, "Timer: " + Math.Ceiling(timer).ToString(), new Vector2(3, 40), Color.White);
+
+            if (timer > 0)
+            {
+              _spriteBatch.Draw(targetSprite, new Vector2(targetPosition.X - targetRadius, targetPosition.Y - targetRadius), Color.White);
+            }
+
+            _spriteBatch.Draw(crosshairsSprite, new Vector2(mState.X - 25, mState.Y - 25), Color.White);
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
